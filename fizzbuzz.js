693e9c858eb7310c83cdda3cc49289ff3fa9d998
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-03-31 19:25:35 -0500

    project working!

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,16 +1,31 @@
-let printfizzbuzz = function(message, num, denom) {
-	if (num % denom == 0) {
-		console.log(message);
-	}
-}
-function fizzbuzz(){
-	for (let i = 0; i <=100; i++){
-		if (i !== 3 ||5){
-			console.log(i);
-		}
-		printfizzbuzz('fizz', i, 3);
-		printfizzbuzz('buzz', i, 5);
+let fbObArr = [{
+	denom: 3,
+	message: 'fizz'
+},{
+	denom:5,
+	message: 'buzz'
+}];
 
+
+
+let fizzbuzz = function(arr){
+	var returnValue= [];
+	let denomArray = arr.map(x => x.denom);
+	let messageArray = arr.map(x => x.message);
+	let counter;
+
+	for (let i = 1; i <=100; i++){
+		counter = i;
+		var value = false;
+		for(let j = 0; j <= arr.length ; j++){
+			if(i % denomArray[j] == 0){
+				console.log(messageArray[j]);
+				value = true;
+			} 
+		}
+		if(value == false){
+			console.log(counter);
+		};
 	}
 }
-fizzbuzz();
+fizzbuzz(fbObArr);
